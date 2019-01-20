8ccbbc086fad43c4a826710acd283750545312f7
vic3king <vic3coorp@gmail.com>
2019-01-20 04:21:46 +0000

    bug(): fix alignment issues and also fix javascript display issues.
    - use flex box to fix alignment issues on the main header
    - add some javascript code to the admin page to enable rendering of candidates currently running for a particular offiice
    
    [delivers #163344226 #163346644]

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -26,3 +26,37 @@ for (let i = 0; i < updateAccord.length; i += 1) {
     }
   });
 }
+
+// reusable conditional statement
+const conditional = (e, btnId, elClass, state) => {
+  // the div wont have an id of btn1, so js will check the next element which will be the button
+  if (e.target.id === btnId) {
+  // effect to make sure only one form is being displayed at a time
+    document.querySelector(elClass).style.display = state;
+  } else {
+    document.querySelector(elClass).style.display = 'none';
+  }
+};
+
+function dislayRedForm() {
+  const lgc = document.querySelector('#lgc');
+  const houseReps = document.querySelector('#house-of-reps');
+  const senateHouse= document.querySelector('#senate-house');
+
+  function displayForm1(e) {
+    // conditional logic function
+    conditional(e, 'lgc', '.candidates', 'block');
+    e.stopPropagation();
+    conditional(e, 'house-of-reps', '.candidates1', 'block');
+    e.stopPropagation();
+    conditional(e, 'senate-house', '.candidates2', 'block');
+    e.stopPropagation();
+  }
+  // event listeners placed on divs
+  lgc.addEventListener('click', displayForm1);
+  houseReps.addEventListener('click', displayForm1);
+  senateHouse.addEventListener('click', displayForm1);
+
+}
+
+dislayRedForm();
