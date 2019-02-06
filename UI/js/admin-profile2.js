3afe043b4e45dfa95b9700711f25d2b44d53df50
vic3king <vic3coorp@gmail.com>
2019-02-06 21:17:38 +0000

    test

--- UI/js/admin-profile2.js
+++ UI/js/admin-profile2.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-unused-expressions */
 const invalidToken = () => {
   window.location = './login.html';
@@ -7,6 +9,12 @@ const { politicoUser, politicoToken } = localStorage;
 if (!politicoToken) {
   invalidToken();
 }
+const logout = () => {
+  localStorage.removeItem('politicoToken');
+  window.location = './index.html';
+};
+
+document.getElementById('logout').addEventListener('click', logout);
 
 const currApiEndpoint = 'http://127.0.0.1:3000/api/v1';
 
@@ -37,16 +45,63 @@ const displayDiv = office => `<div class="div1">
 <button id="lgc" class="veiwrecord vote1">View</button>
 </div>`;
 
-const displayDiv2 = party => `<div class="div1 vote party">
-<div class="party__image2"></div>
-<div class="party__details">
-  <div><b>Party</b>: ${party.name}</div>
-  <div><b>Members</b>: 00</div>
-  <div><b>Hq address</b>: ${party.hqaddress}</div>
-  <button class="update-accord">Edit</button>
-  <button class="delete-record">Delete</button>
-  </div>
-</div>`;
+// const displayDiv2 = (party) => {
+//   console.log(party.id);
+//   return `<div class="div1 vote party">
+// <div class="party__image2"></div>
+// <div class="party__details">
+//   <div><b>Party</b>: ${party.name}</div>
+//   <div><b>Members</b>: 00</div>
+//   <div><b>Hq address</b>: ${party.hqaddress}</div>
+//   <button id="edit-name" class="edit-btn" onclick="updateModal()">Edit</button>
+//   <button class="delete-record">Delete</button>
+//   </div>
+// </div>
+// <div id="myEditModal" class="modal">
+// <section class="modal-content-edit">
+//   <form style="color: white">
+//       <h3 class="page-title3">Update Party Details:</h3>
+//         <label for="title">Name: </label>
+//         <input type="text" placeholder="Update your party name?" name="name" required>
+//           <button type="submit" id="party-name" onclick="handleUpdates(${party.id})">Update</button>
+//     </div>
+//   </form>
+// </section>
+// </div>
+// `;
+// };
+function updateModal() {
+  const modal = document.querySelector('.modal');
+  const modal1 = document.querySelector('.modal1');
+
+  const modalNo = document.querySelectorAll('#modalno');
+  const updateForm = document.querySelectorAll('.edit-btn');
+  const deleteBtn = document.querySelectorAll('.delete-record');
+  modalNo.forEach((button) => {
+    button.onclick = () => {
+      modal1.style.display = 'none';
+    };
+  });
+  deleteBtn.forEach((button) => {
+    button.onclick = () => {
+      modal1.style.display = 'block';
+    };
+  });
+  updateForm.forEach((button) => {
+    button.onclick = () => {
+      modal.style.display = 'block';
+    };
+  });
+
+  window.onclick = (event) => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+    if (event.target === modal1) {
+      modal1.style.display = 'none';
+    }
+  };
+}
 
 fetch(`${currApiEndpoint}/offices`, getOfficesConfig)
   .then(resp => resp.json())
@@ -74,9 +129,79 @@ fetch(`${currApiEndpoint}/parties`, getOfficesConfig)
     }
     let output = '';
     data.forEach((party) => {
-      output += displayDiv2(party);
+      const { id, name, hqaddress } = party;
+      output += `<div class="div1 vote party">
+      <div class="party__image2"></div>
+      <div class="party__details">
+        <div><b>Party</b>: ${name}</div>
+        <div><b>Members</b>: 00</div>
+        <div><b>Hq address</b>: ${hqaddress}</div>
+        <button id="edit-name" class="edit-btn" onclick="">Edit</button>
+        <button class="delete-record">Delete</button>
+        </div>
+      </div>
+      <div id="myEditModal" class="modal">
+      <section class="modal-content-edit">
+        <form style="color: white">
+            <h3 class="page-title3">Update Party Details:</h3>
+              <label for="title">Name: </label>
+              <input type="text" id="party-name" placeholder="Update your party name?" required>
+                <button type="submit" class="update-btn" onclick="handleUpdates(event, ${id})">Update</button>
+          </div>
+        </form>
+      </section>
+      </div>
+      <div id="myModal" class="modal1">
+            <section class="modal-content">
+              <p>Are you sure you want to delete?</p>
+              <button id="modalyes" onclick="handleDelete(${id})" class="modalbtn">Yes</button>
+              <button id="modalno" class="modalbtn2">No</button>
+            </section>
+           </div> 
+      `;
     });
 
     const div = document.querySelector('#grid2');
     div.innerHTML = output;
+
+    updateModal();
   });
+
+function handleDelete(id) {
+  const fetchConfig = {
+    method: 'DELETE',
+    headers: setUpHeader(),
+  };
+  fetch(`${currApiEndpoint}/parties/${id}`, fetchConfig)
+    .then(resp => resp.json())
+    .then((resp) => {
+      console.log(resp);
+      window.location = './admin-profile.html';
+    });
+}
+
+function handleUpdates(event, id) {
+  event.preventDefault();
+  const partyNameEl = document.getElementById('party-name');
+  const formData = {};
+  if (partyNameEl.value) {
+    formData.name = partyNameEl.value;
+  }
+  console.log(id);
+
+  const fetchConfig = {
+    method: 'PATCH',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'x-access-token': politicoToken,
+    },
+    body: JSON.stringify(formData),
+  };
+  fetch(`${currApiEndpoint}/parties/${id}/name`, fetchConfig)
+    .then(resp => resp.json())
+    .then((resp) => {
+      console.log(resp);
+      // window.location = './profile.html';
+    }).catch(err => console.log(err));
+}
--- UI/js/admin.js
+++ UI/js/admin.js
@@ -1,65 +1,66 @@
-const modal2 = document.getElementById('myEditModal');
-const updateForm = document.querySelector('.update-accord');
+// const modal2 = document.getElementById('myEditModal');
+// const updateForm = document.querySelector('.update-accord');
 
-updateForm.onclick = () => {
-  modal2.style.display = 'block';
-};
+// // updateForm.onclick = () => {
+// //   modal2.style.display = 'block';
+// // };
 
-// reusable conditional statement
-const conditional = (e, btnId, elClass, state) => {
-  // the div wont have an id of btn1, so js will check the next element which will be the button
-  if (e.target.id === btnId) {
-  // effect to make sure only one form is being displayed at a time
-    document.querySelector(elClass).style.display = state;
-  } else {
-    document.querySelector(elClass).style.display = 'none';
-  }
-};
+// // reusable conditional statement
+// const conditional = (e, btnId, elClass, state) => {
+//   // the div wont have an id of btn1, so js will check the next element which will be the button
+//   if (e.target.id === btnId) {
+//   // effect to make sure only one form is being displayed at a time
+//     document.querySelector(elClass).style.display = state;
+//   } else {
+//     document.querySelector(elClass).style.display = 'none';
+//   }
+// };
 
-function dislayRedForm() {
-  const lgc = document.querySelector('#lgc');
-  const houseReps = document.querySelector('#house-of-reps');
-  const senateHouse = document.querySelector('#senate-house');
+// function dislayRedForm() {
+//   const lgc = document.querySelector('#lgc');
+//   const houseReps = document.querySelector('#house-of-reps');
+//   const senateHouse = document.querySelector('#senate-house');
 
-  function displayForm1(e) {
-    // conditional logic function
-    conditional(e, 'lgc', '.candidates', 'flex');
-    e.stopPropagation();
-  }
-  // event listeners placed on divs
-  lgc.addEventListener('click', displayForm1);
-  houseReps.addEventListener('click', displayForm1);
-  senateHouse.addEventListener('click', displayForm1);
-}
+//   function displayForm1(e) {
+//     // conditional logic function
+//     conditional(e, 'lgc', '.candidates', 'flex');
+//     e.stopPropagation();
+//   }
+//   // event listeners placed on divs
+//   lgc.addEventListener('click', displayForm1);
+//   houseReps.addEventListener('click', displayForm1);
+//   senateHouse.addEventListener('click', displayForm1);
+// }
 
-// Get the modal
-const modal = document.getElementById('myModal');
-const btn1 = document.getElementById('modalno');
-const btn2 = document.getElementById('modalyes');
-// Get the button that opens the modal
-const btn = document.getElementById('myBtn');
+// // Get the modal
+// const modal = document.getElementById('myModal');
+// const btn1 = document.getElementById('modalno');
+// const btn2 = document.getElementById('modalyes');
+// // Get the button that opens the modal
+// const btn = document.getElementById('myBtn');
 
-// When the user clicks the button, open the modal
-btn.onclick = () => {
-  modal.style.display = 'block';
-};
+// // When the user clicks the button, open the modal
+// btn.onclick = () => {
+//   modal.style.display = 'block';
+// };
 
-// When the user clicks on <span> (x), close the modal
-btn1.onclick = () => {
-  modal.style.display = 'none';
-};
+// // When the user clicks on <span> (x), close the modal
+// btn1.onclick = () => {
+//   modal.style.display = 'none';
+// };
 
-btn2.onclick = () => {
-  document.getElementById('del-test').style.display = 'none';
-};
+// btn2.onclick = () => {
+//   document.getElementById('del-test').style.display = 'none';
+// };
 
-// When the user clicks anywhere outside of the modal, close it
+// // When the user clicks anywhere outside of the modal, close it
+
+const modal = document.querySelector('.modal');
+
 window.onclick = (event) => {
   if (event.target === modal) {
     modal.style.display = 'none';
-  } else if (event.target === modal2) {
-    modal2.style.display = 'none';
   }
 };
 
-dislayRedForm();
+// dislayRedForm();
